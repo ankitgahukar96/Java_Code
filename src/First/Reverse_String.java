6c4436a7883d500cc6bae4505d3d7cb209913862
Ankit Gahukar <topdacankit96@gmail.com>
2023-01-04 13:48:44 +0530

    first commit

---
+++ src/First/Reverse_String.java
@@ -0,0 +1,21 @@
+package First;
+
+public class Reverse_String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+		String str = "Ankit";
+		String reverse = "";
+		
+		System.out.println(str.length());
+		
+		for(int i=str.length()-1;i>=0;i--)
+		{
+			reverse = reverse + str.charAt(i);
+		}
+		
+		System.out.println(reverse);
+	}
+
+}
