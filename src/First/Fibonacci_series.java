dd0e3a13a4fb1c2fa4a92f3f34b95b432ee731ec
Ankit Gahukar <topdacankit96@gmail.com>
2023-01-06 12:12:12 +0530

    fibonnaci updated using scanner

--- src/First/Fibonacci_series.java
+++ src/First/Fibonacci_series.java
@@ -1,18 +1,23 @@
 package First;
 
+import java.util.Scanner;
+
 public class Fibonacci_series {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=1,b=1;
+		Scanner sc = new Scanner(System.in);
 		int k =0;
+		System.out.println("Enter a value");
+		int n = sc.nextInt();
 
-		System.out.println(a+ " " + b);
+		System.out.print(a+ " " + b);
 		
-		while(k<=50)
+		while(k<=n)
 		{
 			k = a+b;
-			System.out.println(k);
+			System.out.print(" " +k);
 			 a =b;
 			 b=k;
 		}
